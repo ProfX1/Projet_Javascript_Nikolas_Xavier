9f7d13850f323a0d339f367e041ff18dd1c0976f
nikbas <nikbasque1@gmail.com>
2024-03-13 19:51:16 -0300

    javatry1

---
+++ JavaScript/monjs.js
@@ -0,0 +1,25 @@
+function loadJSON(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open('GET', 'pass.json', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.send(null);
+}
+
+function handleJSON(response) {
+    var jsonData = JSON.parse(response);
+    console.log(jsonData);
+
+   
+    jsonData.forEach(function (user) {
+        console.log("Username:", user.username);
+        console.log("Password:", user.password);
+        console.log("Flagged:", user.flagged);
+    });
+}
+
+loadJSON(handleJSON);
