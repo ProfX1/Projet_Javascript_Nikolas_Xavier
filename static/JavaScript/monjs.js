1229274d4eef2d84cefc9b62a2178de4bb13f446
Xavier Viel <Xavier.s.viel@gmail.com>
2024-03-19 15:09:38 -0300

    vigenere cypher in js

--- static/JavaScript/monjs.js
+++ static/JavaScript/monjs.js
@@ -1,3 +1,38 @@
+key ="gattaca"
+function vigenereCipher(text, key, mode) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+
+    // Normalize key
+    key = key.toUpperCase().replace(/[^A-Z]/g, '');
+
+    for (let i = 0, j = 0; i < text.length; i++) {
+        const char = text.charAt(i).toUpperCase();
+        let shift = alphabet.indexOf(key.charAt(j % key.length));
+
+        if (alphabet.indexOf(char) !== -1) {
+            if (mode === 'encrypt') {
+                result += alphabet.charAt((alphabet.indexOf(char) + shift) % alphabet.length);
+            } else if (mode === 'decrypt') {
+                result += alphabet.charAt((alphabet.indexOf(char) - shift + alphabet.length) % alphabet.length);
+            }
+            j++;
+        } else {
+            result += char;
+        }
+    }
+
+    return result;
+}
+// Example usage:
+// const plaintext = 'HELLO';
+// const key = 'gattaca';
+// const ciphertext = vigenereCipher(plaintext, key, 'encrypt');
+// console.log('Encrypted:', ciphertext);
+
+// const decryptedText = vigenereCipher(ciphertext, key, 'decrypt');
+// console.log('Decrypted:', decryptedText);
+
 function loadJSON(callback) {
     var xhr = new XMLHttpRequest();
     xhr.overrideMimeType("application/json");
@@ -39,7 +74,11 @@ loadJSON(handleJSON);
 function enter(username, password) {
     var userName = document.getElementById("username").value;
     var Password = document.getElementById("password").value;
+    var cryptedPassword = vigenereCipher(Password, key, 'encrypt')
 
+    if (cryptedPassword == 'Hello World') {
+        document.write("hello")
+    }
   
     var newUser = {
         "username": userName,
