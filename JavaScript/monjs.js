0d56fd8f014ee4017c7bf473507914e843c95fe0
nikbas <nikbasque1@gmail.com>
2024-03-26 13:42:12 -0300

    testcss

---
+++ JavaScript/monjs.js
@@ -0,0 +1,37 @@
+function loadJSON(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open('GET', 'JSON/pass.json', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.send(null);
+}
+
+function handleJSON(response) {
+    var jsonData = JSON.parse(response);
+    console.log(jsonData);
+
+   
+    jsonData.forEach(function (user) {
+        console.log("Username:", user.username);
+        console.log("Password:", user.password);
+        console.log("Flagged:", user.flagged);
+        
+        
+    })
+    
+    ;
+}
+
+loadJSON(handleJSON);
+
+function enter(username, password){
+
+    var userName = document.getElementById("username");
+    var Password = document.getElementById("password");
+    window.alert(userName.value + " " + Password.value);
+    // window.alert(Password.value);
+};
